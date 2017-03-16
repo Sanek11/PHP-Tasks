7de0ef679b8d6d108dd9b1f7fd248fae2a8670fb
Sanek11 <shura.karetnikov.89@mail.ru>
2017-03-16 21:09:20 +0300

    another scoop

---
+++ another scoop/another scoop.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Another scoop</title>
+        <body>
+            <?php
+            
+                $scoops = 5;
+                while ($scoops > 0) {
+                    echo("Another scoop!<br>");
+                    $scoops --;
+                }
+                echo("Life without ice cream isn't the same");
+            
+        </body>
+    </head>
+</html>
